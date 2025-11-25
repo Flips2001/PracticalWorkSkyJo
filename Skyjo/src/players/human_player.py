cf0326b9d6c9e63b06c90d6d1701fd3ab923a218
Phillip Lechenauer <phillip.uni@lechenauer.com>
2025-11-25 11:41:37 +0100

    add docstring

--- Skyjo/src/players/human_player.py
+++ Skyjo/src/players/human_player.py
@@ -2,6 +2,19 @@ from players.player import Player
 
 class HumanPlayer(Player):
     def select_action(self, observation, legal_actions):
+        """
+        Prompt the human player to select an action from the list of legal actions.
+        Displays the player's current grid, the top of the discard pile, and the available legal actions.
+        Prompts the user to input the number corresponding to their chosen action.
+        Args:
+            observation: An object containing the current state of the game relevant to the player.
+            legal_actions: A list of actions that the player is allowed to take on this turn.
+        Returns:
+            The action selected by the player from the list of legal actions.
+        Raises:
+            ValueError: If there are no legal actions available to select from.
+        """
+
         if not legal_actions:
             raise ValueError("No legal actions available to select from.")
 
--- Skyjo/src/players/random_player.py
+++ Skyjo/src/players/random_player.py
@@ -3,6 +3,17 @@ import random
 
 class RandomPlayer(Player):
     def select_action(self, observation, legal_actions):
+        """
+        Randomly selects an action from the list of legal actions.
+        Args:
+            observation: An object containing the current state of the game relevant to the player.
+            legal_actions: A list of actions that the player is allowed to take on this turn.
+        Returns:
+            The action randomly selected from the list of legal actions.
+        Raises:
+            ValueError: If there are no legal actions available to select from.
+        """
+
         if not legal_actions:
             raise ValueError("No legal actions available to select from.")
         return random.choice(legal_actions)
