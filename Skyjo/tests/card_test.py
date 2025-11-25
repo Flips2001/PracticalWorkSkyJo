0270ec37ebe4993ac66a329c040ef665f2a13f10
Phillip Lechenauer <phillip.uni@lechenauer.com>
2025-11-25 15:12:54 +0100

    add precommit hook

--- Skyjo/tests/card_test.py
+++ Skyjo/tests/card_test.py
@@ -1,25 +1,31 @@
 import pytest
 from Skyjo.src.card import Card
 
+
 @pytest.fixture
 def card():
     return Card(3)
 
+
 def test_print_card_face_down(card):
     assert repr(card) == "[X]"
+
 
 def test_print_card_face_up(card):
     card.reveal()
     assert repr(card) == "[3]"
+
 
 def test_reveal_card(card):
     card.reveal()
     assert not card.is_hidden()
     assert card.get_value() == 3
 
+
 def test_get_value_face_down(card):
     with pytest.raises(ValueError):
         card.get_value()
+
 
 def test_hide_card(card):
     card.reveal()
