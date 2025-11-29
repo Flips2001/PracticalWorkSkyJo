00744c7509bcfa31c4ba0ac22790d608d792d8c2
Leokor1 <k11713987@students.jku.at>
2025-11-29 14:37:45 +0100

    implement suggested player_state changes

--- Skyjo/src/player_state.py
+++ Skyjo/src/player_state.py
@@ -1,28 +1,25 @@
+from dataclasses import dataclass
 from Skyjo.src.card import Card
 from typing import List
 
+@dataclass
 class PlayerState:
-    score: int
     player_id: int
-    grid: List[List[Card]]
-    
-    def __init__(self, player_id: int):
-        self.id = player_id
-        self.score = 0
-        self.grid: List[List[Card]] = []
+    __score: int = 0
+    __grid: List[List[Card]] = []
 
     def get_grid(self) -> List[List[Card]]:
-        return self.grid
+        return self.__grid
     
     def calculate_score(self) -> int:
         total_score = 0
-        for row in self.grid:
+        for row in self.__grid:
             for card in row:
                 if card.face_up:
                     total_score += card.value
         self.score = total_score
-        return self.score
+        return self.__score
 
     def reset(self):
-        self.score = 0
-        self.grid = []
+        self.__score = 0
+        self.__grid = []
