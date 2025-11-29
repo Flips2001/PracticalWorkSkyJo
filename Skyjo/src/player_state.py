de8579c9706b2ace1438d9a35289b47db8b62134
Leokor1 <k11713987@students.jku.at>
2025-11-29 13:43:38 +0100

    add player_state class

---
+++ Skyjo/src/player_state.py
@@ -0,0 +1,28 @@
+from Skyjo.src.card import Card
+from typing import List
+
+class PlayerState:
+    score: int
+    player_id: int
+    grid: List[List[Card]]
+    
+    def __init__(self, player_id: int):
+        self.id = player_id
+        self.score = 0
+        self.grid: List[List[Card]] = []
+
+    def get_grid(self) -> List[List[Card]]:
+        return self.grid
+    
+    def calculate_score(self) -> int:
+        total_score = 0
+        for row in self.grid:
+            for card in row:
+                if card and not card.is_hidden:
+                    total_score += card.value
+        self.score = total_score
+        return self.score
+
+    def reset(self):
+        self.score = 0
+        self.grid = []
