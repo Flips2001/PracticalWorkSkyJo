614fa3f1afa185ba19684b767f1672eb940e7623
Phillip Lechenauer <phillip.uni@lechenauer.com>
2026-01-20 16:52:41 +0100

    update competition script

--- Skyjo/competitions/competition.py
+++ Skyjo/competitions/competition.py
@@ -8,19 +8,20 @@ NUM_GAMES = 1000
 if __name__ == "__main__":
     player1 = RandomPlayer(player_id=0, player_name="Random")
     player2 = PhillipsPlayer(player_id=1, player_name="Phillips")
+    players = [player1, player2]
 
     total_scores = [0, 0, 0]
     wins = [0, 0, 0]
 
     for i in tqdm(range(NUM_GAMES)):
         game = SkyjoGame()
-        game.add_player(player1)
-        game.add_player(player2)
+        for player in players:
+            game.add_player(player)
 
         game.play_game()
 
         scores = game.game_state.all_player_final_scores
-        for j in range(3):
+        for j in range(len(players)):
             total_scores[j] += scores[j]
 
         # Winner is the one with the lowest score
@@ -28,8 +29,7 @@ if __name__ == "__main__":
         wins[winner_id] += 1
 
     print(f"\nAfter {NUM_GAMES} games:")
-    players = [player1, player2]
-    for j in range(3):
+    for j in range(len(players)):
         print(
             f"{players[j].player_name}: Total Score: {total_scores[j]}, Average: {total_scores[j] / NUM_GAMES:.2f}, Wins: {wins[j]}"
         )
